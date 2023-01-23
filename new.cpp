42a00bd1a1a5dd4888ca99bdf54482119655eda7
Arpan Thapa <thearpanth@gmail.com>
2023-01-23 06:06:28 -0500

    changes using endl

--- new.cpp
+++ new.cpp
@@ -1,8 +1,8 @@
- #include <iostream>
+#include<iostream>
 using namespace std;
 
 int main() {
-  cout << "Hello World! \n";
-  cout << "I am learning C++";
-  return 0;
-} 
+    cout<<"hello world"<< endl;
+    cout<<"i am learning c++";
+    return 0;
+}
