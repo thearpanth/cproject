73d64d6efb566ac7711ffa7c5a111de32ef9948d
Arpan Thapa <thearpanth@gmail.com>
2023-01-24 12:50:24 -0500

    array input

--- new.cpp
+++ new.cpp
@@ -3,10 +3,17 @@
 using namespace std;
 
 int main() {
-    string a[]={"apple","banana","chesternut","dragon fruit","eddo"};
-
-    for(int i=0;i<5;i++) {
-        cout<< a[i] <<"\n";
+    string b[5];
+    b[0]="apple";
+    b[1]="banana";
+    b[2]="chesternut";
+    b[3]="dragon fruit";
+    b[4]="eddo fruit";
+    
+    for(int i=0; i<5 ; i++){
+        cout<<b[i]<<"\n";
     }
-return 0;
+    return 0;
+
+    return 0;
 }
