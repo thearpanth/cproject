5436dd3d68dacf80f7591c3acf67cee1ddd2be8d
Arpan Thapa <thearpanth@gmail.com>
2023-01-24 07:03:43 -0500

    using pass and contine

--- runner.cpp
+++ runner.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
-
+//i am using pass and continue 
 int main() {
-cout<<"hello runners";
-return 0;
-} 
+    for(int i=1; i<10; i++) {
+        if(i==6){
+            cout<<"its ends now"<<"\n";
+            break;
+        }
+        else if(i==3){
+            cout<<"i pass at 3"<<"\n";
+            continue;
+        }
+        else if(i!=3) {
+            cout<<"else the num is not 3 it is "<<i<<" hi"<<"\n";
+
+        }
+    }
+}
