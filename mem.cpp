e82ffd4f7b7cbdeb0c5a47b3a7bf32a9a72849cb
Arpan Thapa <thearpanth@gmail.com>
2023-01-26 10:19:52 -0500

    used all about ref

--- mem.cpp
+++ mem.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
-using namespace std;
 #include<string>
+using namespace  std;
 
 int main() {
-    string food="arpan";
-    string &meme=food;
-    string *ptr = &food;
+    string car="ford";
+    string &a=car;
+    string *str = &car;
 
-    cout<<food<<"\n";
-    cout<<meme<<"\n";
-    cout<<ptr<<"\n";
-
-    cout<<*ptr<<"\n"; //deferancing 
-    
-return 0;
+    cout<<car<<" print what in the car"<<"\n";
+    cout<<a<<" what in the referencing"<<"\n";
+    cout<<str<<" whats inside str"<<"\n";
+    cout<<*str<<" whats inside *str"<<"\n";
+    *str="tesla";
+    cout<<*str<<" i changed it value of *str"<<"\n";
+    cout<<car<<" the calue of car is "<<"\n";
+    cout<<a<<" the new value of a is "<<"\n";
+    cout<<str<<" whats inside str"<<"\n";
 }
