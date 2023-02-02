d096ba4639b442c428a39336ae0f6309716a1b02
Arpan Thapa <thearpanth@gmail.com>
2023-02-02 07:27:58 -0500

    it something more misunderstanding

--- opp.cpp
+++ opp.cpp
@@ -15,9 +15,27 @@ class car: public veh {
     string model = "mustang";
 };
 
+class veh1: public veh {
+    public:
+    void myfun(){
+        cout << "its better than veh0 \n";
+    }
+};
+
+class veh2: public veh1 {
+    public:
+    void myfun1(){
+        cout << "its improved than veh1 \n";
+    }
+};
+
 int main() {
     car mycar;
+    veh1 mycar1;
+    veh2 mycar2;
     mycar.honk();
-    cout << mycar.brand + " " + mycar.model;
+    cout << mycar.brand + " " + mycar.model<<"\n";
+    mycar1.myfun();
+    mycar2.myfun1();
     return 0;
 }
