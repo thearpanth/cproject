723915f295a3aa05578c005a7918548242c8b693
Arpan Thapa <thearpanth@gmail.com>
2023-02-05 17:47:46 +0545

    error

--- new.cpp
+++ new.cpp
@@ -1,22 +1,17 @@
-#include<string>
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-    string b[5];
-    b[0]="apple";
-    b[1]="banana";
-    b[2]="chesternut";
-    b[3]="dragon fruit";
-    b[4]="eddo fruit";
-    
-    for(int i=0; i<5 ; i++){
-        cout<<b[i]<<"\n";
+  try {
+    int age = 15;
+    if (age >= 18) {
+      cout << "Access granted - you are old enough.";
+    } else {
+      throw 505;
     }
-
-    cout<<"the size of string is "<<sizeof(string)<<endl;
-    cout<<"the size of the string is \"b\" "<<sizeof(b)<<endl; //i am using singe slash 
-    return 0;
-
-    return 0;
+  }
+  catch (...) {
+    cout << "Access denied - You must be at least 18 years old.\n";
+  }
+  return 0;
 }
