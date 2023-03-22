351929422c1f0f816ac9905538012654fccdd1a4
Arpan Thapa <thearpanth@gmail.com>
2023-03-22 16:07:25 +0545

    task 6

--- task.cpp
+++ task.cpp
@@ -32,28 +32,45 @@ class BankAccount{
 };
 
 int main(){
+    int i=1;
+    int ba=0;
+    char opt1;
 
-    BankAccount account1("A1",1000);
-    char opt;
-    int dep, with;
-    cout << "enter what you wan do" ;
-    cin >> opt;
-
-    if(opt=='a'){
-        cout << "the balance account is " << account1.balance << endl; 
-    } 
-    else if(opt=='b'){
-        cout << "enter the amount to enter " ;
-        cin >> dep;
-        account1.deposit(dep);
-    }
-    else if(opt=='c'){
-        cout << "enter the withdrawn ammount" ;
-        cin >> with;
-        account1.withdraw(with);
+    while(i<=1){
+    
+        BankAccount account1("A1",ba);
+         char opt;
+         int dep, with;
+         cout << "enter what you wan do " ;
+         cin >> opt;
+         if(opt=='a'){
+            cout << "the balance account is " << account1.balance << endl; 
+        } 
+        else if(opt=='b'){
+            cout << "enter the amount to enter " ;
+            cin >> dep;
+            account1.deposit(dep);
+        }
+        else if(opt=='c'){
+            cout << "enter the withdrawn ammount " ;
+            cin >> with;
+            account1.withdraw(with);
+        }
+        else{
+        cout << "invaid";
+        }
+    
+    cout << endl << "your new balance is " << account1.balance;
+    ba = account1.balance;
+    
+    cout << endl << "do you wana continue ";
+    cin >> opt1;
+    if (opt1=='y'){
+        cout << endl << "sure" << endl;
     }
     else{
-        cout << "invaid";
+        i=i+1;
     }
-
+    } 
+return 0;
 }
