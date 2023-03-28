53b36e5304d42573a99ab5a3bdb337a4cf7007e9
Arpan Thapa <thearpanth@gmail.com>
2023-03-28 14:38:11 +0545

    task 7

--- task.cpp
+++ task.cpp
@@ -1,76 +1,37 @@
-//Create a class called "BankAccount" with properties such as balance and account number. Create methods in the "BankAccount" class to deposit and withdraw money.
+//Create a class called "Fraction" with properties such as numerator and denominator. Create a method in the "Fraction" class that adds two fractions together.
 
 #include<iostream>
-#include<string>
 using namespace std;
 
-class BankAccount{
-    public:
-    int balance;
-    string id;
-    BankAccount(string a,int b){
-        balance = b;
-        id = a;
-    }
-
-    int deposit(int dep){
-        balance += dep;
-        return balance;
-    }
-
-    int withdraw(int with){
-        if(balance>=with){
-            balance-=with;
-        }
-        else{
-            cout << "not sufficaint amount of money" << endl;
-        }
-        return balance;
-    }
+class fra{
     
+    public:
+    int n0,d0,n1,d1,n2,d2;
+    fra(int a,int b,int c, int d){
+        n1=a , d1 = b , n2 = c , d2 = d;
+    }
 
+     void cal(){
+        n0 = (d1*n2)+(d2*n1);
+        d0 = d1*d2;
+        int i;
+        int a = 0;
+        for(i=2;i<=10;i++){
+            while(a<1){     
+                if(d0%i==0 && n0%i==0){
+                    d0=d0/i;
+                    n0=n0/i;
+                }
+                else{
+                  a++;
+                }
+            }
+        }
+     }
 };
 
 int main(){
-    int i=1;
-    int ba=0;
-    char opt1;
-
-    while(i<=1){
-    
-        BankAccount account1("A1",ba);
-         char opt;
-         int dep, with;
-         cout << "enter what you wan do " ;
-         cin >> opt;
-         if(opt=='a'){
-            cout << "the balance account is " << account1.balance << endl; 
-        } 
-        else if(opt=='b'){
-            cout << "enter the amount to enter " ;
-            cin >> dep;
-            account1.deposit(dep);
-        }
-        else if(opt=='c'){
-            cout << "enter the withdrawn ammount " ;
-            cin >> with;
-            account1.withdraw(with);
-        }
-        else{
-        cout << "invaid";
-        }
-    
-    cout << endl << "your new balance is " << account1.balance;
-    ba = account1.balance;
-    
-    cout << endl << "do you wana continue ";
-    cin >> opt1;
-    if (opt1=='y'){
-        cout << endl << "sure" << endl;
-    }
-    else{
-        i=i+1;
-    }
-    } 
-return 0;
+    fra ca(1,2,1,2);
+    ca.cal();
+    cout << ca.n0 << "/" << ca.d0 << endl;    
 }
